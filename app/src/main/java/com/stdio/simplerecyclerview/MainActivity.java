1302fef48b868e5d64a50e5488c0c86444e5033d
Std1o <37378410+Std1o@users.noreply.github.com>
2021-12-24 09:57:54 +0300

    upd

--- app/src/main/java/com/stdio/simplerecyclerview/MainActivity.java
+++ app/src/main/java/com/stdio/simplerecyclerview/MainActivity.java
@@ -12,31 +12,32 @@ public class MainActivity extends AppCompatActivity {
 
     private ArrayList<DataModel> dataList;
     private RecyclerView rv;
+    RVAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        initRecyclerView();
+        initRV();
     }
 
-    private void initRecyclerView() {
+    private void initRV() {
         rv = findViewById(R.id.rv);
         LinearLayoutManager llm = new LinearLayoutManager(this);
         rv.setLayoutManager(llm);
-        initializeData();
-        initializeAdapter();
+        fillData();
+        initAdapter();
     }
 
-    private void initializeData() {
+    private void fillData() {
         dataList = new ArrayList<>();
         dataList.add(new DataModel(getString(R.string.news_header_top_stories), getString(R.string.news_body1)));
         dataList.add(new DataModel(getString(R.string.news_header_world), getString(R.string.news_body4)));
         dataList.add(new DataModel(getString(R.string.news_header_australia), getString(R.string.news_body6)));
     }
 
-    private void initializeAdapter() {
-        RVAdapter adapter = new RVAdapter(dataList, this);
+    private void initAdapter() {
+        adapter = new RVAdapter(dataList, this);
         rv.setAdapter(adapter);
     }
 }
